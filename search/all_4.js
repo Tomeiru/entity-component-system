89b9780a86f5c47f7af6db2d58bd979569a7f926
Tomeiru <71699948+Tomeiru@users.noreply.github.com>
2023-02-05 16:14:26 +0000

    Deploying to gh-pages from @ Tomeiru/entity-component-system@b7b307407df05613f33669b4705e5745bd20e8eb 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['getcomponenttype_1',['getComponentType',['../class_e_c_s_1_1_component_manager.html#a8c637fdb27eb613f751a62bff6a05123',1,'ECS::ComponentManager::getComponentType()'],['../class_e_c_s_1_1_coordinator.html#a626b637f55cc766160f4974c97d29f21',1,'ECS::Coordinator::getComponentType()']]],
   ['getdata_2',['getData',['../class_e_c_s_1_1_component_array.html#ac5d4dad139e1461330d76436c752949f',1,'ECS::ComponentArray']]],
   ['getresource_3',['getResource',['../class_e_c_s_1_1_coordinator.html#a6f1a1ddcd97414a211dcb7924b368419',1,'ECS::Coordinator::getResource()'],['../class_e_c_s_1_1_resource_manager.html#ac783b729e302252fae740e3158025ace',1,'ECS::ResourceManager::getResource()']]],
-  ['getsignature_4',['getSignature',['../class_e_c_s_1_1_entity_manager.html#a06945298f6264950979614a1200dc451',1,'ECS::EntityManager']]]
+  ['getsignature_4',['getSignature',['../class_e_c_s_1_1_entity_manager.html#a06945298f6264950979614a1200dc451',1,'ECS::EntityManager']]],
+  ['getsystem_5',['getSystem',['../class_e_c_s_1_1_coordinator.html#ab572f0884638589680cbe4be7a7226d1',1,'ECS::Coordinator::getSystem()'],['../class_e_c_s_1_1_system_manager.html#abdba65127efce9ef40f070672a12f47b',1,'ECS::SystemManager::getSystem()']]]
 ];
